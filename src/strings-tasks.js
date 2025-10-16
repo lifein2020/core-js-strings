46ffcc1186592a21bd29b030140cfaeccc44e4d0
Svetlana Zh <lady_zhivtsova@gmail.com>
2025-10-16 18:34:22 +0300

    feat: implement functions getStringLength() isString() concatenateStrings() getFirstChar()

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -19,9 +19,10 @@
  *   getStringLength(null) => 0
  *   getStringLength(undefined) => 0
  */
-function getStringLength(/* value */) {
-  throw new Error('Not implemented');
+function getStringLength(value) {
+  return typeof value !== 'string' ? 0 : value.length;
 }
+// getStringLength('Hello');
 
 /**
  * Returns true if the value is a string, otherwise returns false.
@@ -37,9 +38,16 @@ function getStringLength(/* value */) {
  *   isString('test') => true
  *   isString(new String('test')) => true
  */
-function isString(/* value */) {
-  throw new Error('Not implemented');
+
+function isString(value) {
+  return value instanceof String || typeof value === 'string';
 }
+// isString('test');
+// isString();
+// isString(null);
+// isString([]);
+// isString({});
+// isString('test');
 
 /**
  * Returns the result of concatenation of two strings.
@@ -53,8 +61,8 @@ function isString(/* value */) {
  *   concatenateStrings('aa', '') => 'aa'.
  *   concatenateStrings('', 'bb') => 'bb'
  */
-function concatenateStrings(/* value1, value2 */) {
-  throw new Error('Not implemented');
+function concatenateStrings(value1, value2) {
+  return value1.concat('', value2);
 }
 
 /**
@@ -68,8 +76,11 @@ function concatenateStrings(/* value1, value2 */) {
  *   getFirstChar('cat') => 'c'
  *   getFirstChar('') => ''
  */
-function getFirstChar(/* value */) {
-  throw new Error('Not implemented');
+function getFirstChar(value) {
+  if (typeof value === 'string') {
+    return value.charAt(0);
+  }
+  return '';
 }
 
 /**
